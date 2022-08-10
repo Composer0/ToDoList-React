b55f2fcc8dae397597c51bcc1c5deeef3ffcbfcb
Orion Palmer <97369887+Composer0@users.noreply.github.com>
2022-08-10 12:04:18 -0400

    Updated Version of ToDo List
    ***New features added.
    Instead of having to click "Add" you may now press "Enter" to easily add multiple items to your list!
    It is now possible to remove list items. Just click the 'X' button to remove that particular list item.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,47 +1,88 @@
-import React, {useState} from "react";
+import React, {useState} from 'react';
 
 function App() {
+ 
 
-
-const [item, setItem] = useState("")
-
-const [newItem, setNewItem] = useState([]);
+//Reads the text input    
+const[text, setText] = useState("");
 
 function handleChange(event) {
-  const newValue = event.target.value;
-  setItem(newValue)
+    const newValue = event.target.value;
+    setText(newValue);
 }
+//Reads the text input
 
-function handleClick() {
-  setNewItem((prevValues) => {
-    return [...prevValues, item];
-  })
-  setItem("")
+
+//Adds creates a list item using the input text.
+const[items, setItems] = useState([]);
+
+function addItems() {
+    setItems((prevValues) => {
+        return [...prevValues, text];
+    })
+    setText("")
 }
+//Adds creates a list item using the input text.
 
 
-  return (
-    // <form onChange={handleClick}>
+//Removes Items on Button Click
+function removeItem() {
+    const li = document.querySelectorAll('li');
+    const removeInput = document.querySelectorAll(".remove");
 
-    <div className="container">
-      <div className="heading">
-        <h1>To-Do List</h1>
-      </div>
-      <div className="form">
-      <input onChange={handleChange} type="text" placeholder="Enter Item Here" value={item}
-      />
-        <button onClick={handleClick}>
-          <span>Add</span>
-        </button>
-      </div>
-      <div>
-        <ul>
-          {newItem.map(todoItems => <li>{todoItems}</li>)}
-        </ul>
-      </div>
-    </div>
-    // </form>
-  );
+    for (var index = 0; index < li.length; index++) {
+        removeInput[index].addEventListener("click", function(){
+            this.closest(".lineItem").remove();
+        })
+    }
+}
+//Removes Items on Button Click
+
+//Allows you to press Enter instead of clicking Add to add another list item. Its a convenience feature :D
+function handleKeyPress(e) {
+    if(e.key === "Enter") {
+        addItems()
+    } 
+}
+//Use enter instead of Add.
+
+
+    return (
+        // <form onChange={handleClick}>
+    
+        <div className="container">
+          <div className="heading">
+            <h1>To-Do List</h1>
+          </div>
+          <div className="form">
+            <input 
+                onChange={handleChange}
+                value={text}
+                type="text" required 
+                placeholder="Enter Item Here" 
+                onKeyPress={(e) => handleKeyPress(e)}
+
+            />
+            <button
+            onClick={addItems}
+            >
+              <span>Add</span>
+            </button>
+          </div>
+          <div>
+            <ul>
+               {items.map(todoItems => 
+               
+               <li className="lineItem" >{todoItems}
+               <button className="remove" onClick={removeItem}>(X)</button>
+               </li>)} 
+            </ul>
+          </div>
+        </div>
+        // </form>
+      );
+
+
 }
 
 export default App;
