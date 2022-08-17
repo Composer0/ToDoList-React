d9b0ae4912a53a60cf9be6ef998bd333088773b6
Orion Palmer <97369887+Composer0@users.noreply.github.com>
2022-08-16 23:19:13 -0400

    Deployment Build

---
+++ precache-manifest.69d381cf7681d2bfdbeabfc1c974d034.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2589a17b71820d926e192e2589182550",
+    "url": "/ToDoList-React/index.html"
+  },
+  {
+    "revision": "a29db527bec575b7ac74",
+    "url": "/ToDoList-React/static/js/2.79900b9e.chunk.js"
+  },
+  {
+    "revision": "27e68c1d2de9b1b25fb2",
+    "url": "/ToDoList-React/static/js/main.4aa72748.chunk.js"
+  },
+  {
+    "revision": "e62a32e356a5500aeff5",
+    "url": "/ToDoList-React/static/js/runtime-main.b4770048.js"
+  }
+]);
